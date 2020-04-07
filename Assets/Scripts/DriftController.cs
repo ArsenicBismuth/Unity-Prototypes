3300aa19bf6d93535b80669ea34c1cd92150f45d
Arsenic <d.faris323@gmail.com>
2020-04-07 09:07:42 +0700

    Velocity on rotation
    - Add velocity transfer on rotation.
    - Add ray to visualize velocity.
    - Remove unnecessary assets

--- Assets/Scripts/DriftController.cs
+++ Assets/Scripts/DriftController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class DriftController : MonoBehaviour {
 
     #region Parameters
-    public float Accel = 25.0f;     // In meters/second2
+    public float Accel = 25.0f;         // In meters/second2
     public float Accel2 = 40.0f;
-    public float TopSpeed = 100.0f; // In meters/second
-    public float Jump = 3.0f;       // In meters/second2
-    public float Grip = 3.0f;       // In meters/second2
-    public float Rotate = 360.0f;   // In degree/second
+    public float TopSpeed = 100.0f;     // In meters/second
+    public float Jump = 3.0f;           // In meters/second2
+    public float GripX = 3.0f;          // In meters/second2
+    public float GripZ = 0.5f;          // In meters/second2
+    public float Rotate = 360.0f;       // In degree/second
+    public float RotVelocity = 0.8f;    // Ratio of forward velocity transfered on rotation
 
     // Ground & air angular drag
     // reduce stumbling time on ground but maintain on-air one
@@ -19,18 +21,23 @@ public class DriftController : MonoBehaviour {
     #endregion
 
     #region Intermediate
-    private Rigidbody rigidBody;
+    private Rigidbody rigidbody;
     private Collider boxCollider;
     private float distToGround;
 
+    // The actual value to be used (modification of parameters)
     private float rotate;
     private float accel;
-    private float grip;
+    private float gripX;
+    private float gripZ;
 
+    // For determining drag direction
     private float isRight = 1.0f;
+    private float isForward = 1.0f;
     private float isCW = 1.0f;
 
     private bool isMoving = false;
+    private bool isRotating = false;
 	private bool isBoost = false;
     private bool isGrounded = true;
     private bool isTouch = false;
@@ -42,41 +49,44 @@ public class DriftController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		rigidBody = GetComponent<Rigidbody>();
+		rigidbody = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
         distToGround = boxCollider.bounds.extents.y;   // Pivot to the outermost collider
     }
 
     // Update is called once per frame
     void Update() {
-
+        Debug.DrawRay(transform.position, rigidbody.velocity / 2, Color.green);
     }
 
     // Update is called once multiple times per frame (according to physics setting)
     void FixedUpdate() {
         #region Situational Check
         rotate = Rotate;
-        grip = Grip;
-        rigidBody.angularDrag = AngDragG;
+        gripX = GripX;
+        gripZ = GripZ;
+        rigidbody.angularDrag = AngDragG;
 
         // Adjustment in slope
         accel = accel * Mathf.Cos(transform.localEulerAngles.x * Mathf.Deg2Rad);
         accel = accel > 0f ? accel : 0f;
-        grip = grip * Mathf.Cos(transform.localEulerAngles.z * Mathf.Deg2Rad);
-        grip = grip > 0f ? grip : 0f;
-        Debug.Log(transform.localEulerAngles.z + " " + accel);
+        gripZ = gripZ * Mathf.Cos(transform.localEulerAngles.x * Mathf.Deg2Rad);
+        gripZ = gripZ > 0f ? gripZ : 0f;
+        gripX = gripX * Mathf.Cos(transform.localEulerAngles.z * Mathf.Deg2Rad);
+        gripX = gripX > 0f ? gripX : 0f;
 
         // A short raycast to check below
         isGrounded = Physics.Raycast(transform.position, -transform.up, distToGround + 0.1f);
         if (!isGrounded) {
             rotate = 0f;
             accel = 0f;
-            grip = 0f;
-            rigidBody.angularDrag = AngDragA;
+            gripX = 0f;
+            gripZ = 0f;
+            rigidbody.angularDrag = AngDragA;
         }
 
         // Prevent the rotational input intervenes with physics angular velocity 
-        isStumbling = rigidBody.angularVelocity.magnitude > 0.1f * Rotate * Time.deltaTime;
+        isStumbling = rigidbody.angularVelocity.magnitude > 0.1f * Rotate * Time.deltaTime;
         if (isStumbling) {
             //rotate = 0f;
         }
@@ -88,12 +98,12 @@ public class DriftController : MonoBehaviour {
         isMoving = false;
 
         if (Input.GetAxisRaw("Throttle") > 0.5f || Input.GetAxisRaw("Throttle") < -0.5f) {
-            rigidBody.velocity += transform.forward * Input.GetAxisRaw("Throttle") * accel * Time.deltaTime;
-            isMoving = true;
+            rigidbody.velocity += transform.forward * Input.GetAxisRaw("Throttle") * accel * Time.deltaTime;
+            gripZ = 0f;     // Remove straight grip if wheel is rotating
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            rigidBody.velocity += transform.up * accel * Time.deltaTime;
+            rigidbody.velocity += transform.up * accel * Time.deltaTime;
         }
 
         if (Input.GetKeyDown(KeyCode.R)) {  // Reset
@@ -110,13 +120,14 @@ public class DriftController : MonoBehaviour {
         }
 
         // Get the local-axis velocity before rotation (+x, +y, and +z = right, up, and forward)
-        Vector3 pvel = transform.InverseTransformDirection(rigidBody.velocity);
+        Vector3 pvel = transform.InverseTransformDirection(rigidbody.velocity);
+
+        isRotating = false;
 
         // Turn by keyboard
         if (Input.GetAxisRaw("Sideways") > 0.5f || Input.GetAxisRaw("Sideways") < -0.5f) {
             isCW = Input.GetAxisRaw("Sideways");
             rotate_grad_key();
-            isMoving = true;
         }
 
         // Turn by facing cursor
@@ -134,27 +145,28 @@ public class DriftController : MonoBehaviour {
 
         #region Passives
         // Get the local-axis velocity after rotation
-        Vector3 vel = transform.InverseTransformDirection(rigidBody.velocity);
+        Vector3 vel = transform.InverseTransformDirection(rigidbody.velocity);
+
+        // Rotate the velocity vector
+        // TODO: Tweak more, still feels strange
+        //vel = pvel;                   // Transfer all
+        if (isRotating) vel = vel * 0.5f + pvel * 0.5f; // Partial transfer
 
         // Sideway grip
         isRight = vel.x > 0f ? 1f : -1f;
-        vel.x -= isRight * grip * Time.deltaTime;   // Accelerate in opposing direction
+        vel.x -= isRight * gripX * Time.deltaTime;   // Accelerate in opposing direction
         if (vel.x * isRight < 0f) vel.x = 0f;       // Check if changed polarity
 
-        // TODO: Transfer some to forward momentum (in the new direction)
-        //vel.z = pvel.z;
-
-        // Backward grip
-        if (vel.z < -1f) {
-            vel.z += grip/2 * Time.deltaTime;
-            if (vel.z > 0f) vel.z = 0f;
-        }
+        // Straight grip
+        isForward = vel.z > 0f ? 1f : -1f;
+        vel.z -= isForward * gripZ * Time.deltaTime;
+        if (vel.z * isForward < 0f) vel.z = 0f;
 
         // Top speed
         if (vel.z > TopSpeed) vel.z = TopSpeed;
         else if (vel.z < -TopSpeed) vel.z = -TopSpeed;
 
-        rigidBody.velocity = transform.TransformDirection(vel);
+        rigidbody.velocity = transform.TransformDirection(vel);
         #endregion
 
     }
@@ -186,6 +198,7 @@ public class DriftController : MonoBehaviour {
             rot = transform.eulerAngles;
             rot.y = angle;
             transform.eulerAngles = rot;
+            isRotating = true;
         }
     }
 
@@ -193,6 +206,7 @@ public class DriftController : MonoBehaviour {
         // Delta = right(taget) - left(current)
         drot.y = isCW * rotate * Time.deltaTime;
         transform.rotation *= Quaternion.AngleAxis(drot.y, transform.up);
+        isRotating = true;
     }
 
     void rotate_grad_abs(float angle) {
@@ -207,12 +221,13 @@ public class DriftController : MonoBehaviour {
 
         delta = Mathf.DeltaAngle(AngleOffset(rot.y, 0f), angle);
         if (delta * isCW < 0f) rot.y = angle;       // Check if changed polarity
+        else isRotating = true;
 
         // You can't set them directly as it'll set x & z to zero
         // if you're not using eulerAngles.x & z.
         transform.eulerAngles = rot;
         //transform.rotation = Quaternion.AngleAxis(rot.y, Vector3.up);
-        //rigidBody.MoveRotation(Quaternion.Euler(rot));
+        //rigidbody.MoveRotation(Quaternion.Euler(rot));
     }
 
     void rotate_grad_rel(float angle) {
@@ -227,13 +242,14 @@ public class DriftController : MonoBehaviour {
 
         delta = Mathf.DeltaAngle(AngleOffset(rot.y, drot.y), angle);
         if (delta * isCW < 0f) drot.y = 0;       // Check if changed polarity
+        else isRotating = true;
 
         // Add the drot to current rotation
         transform.rotation *= Quaternion.AngleAxis(drot.y, transform.up);
-        //rigidBody.rotation *= Quaternion.AngleAxis(drot.y, transform.up);
+        //rigidbody.rotation *= Quaternion.AngleAxis(drot.y, transform.up);
         //transform.Rotate(drot, Space.Self);
-        //rigidBody.AddTorque(drot);
-        //rigidBody.MoveRotation(rigidBody.rotation * Quaternion.Euler(drot));
+        //rigidbody.AddTorque(drot);
+        //rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(drot));
     }
     #endregion
 }

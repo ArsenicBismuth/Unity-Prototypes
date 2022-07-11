dad1487575c6724aabea70d14ced4d4b1829c360
Arsenic Bismuth <d.faris323@gmail.com>
2022-07-12 01:07:58 +0200

    Simple Bronchoscopy Simulation
    For a class/lab project (which has simpler requirements).

---
+++ Bronchoscopy/Assets/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    public float speed = 0.1f;
+    public float lookSpeed = 1f;
+    
+    private CharacterController character;
+    private Vector3 trans;
+    private Vector2 rot = Vector2.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        // Rotate according to mouse, FPS
+        rot.y += Input.GetAxis("Mouse X");
+        rot.x += -Input.GetAxis("Mouse Y");
+        rot.x = Mathf.Clamp(rot.x, -90f, 90f);
+        
+        // Spaceship, full xy axis rotation
+        transform.eulerAngles = new Vector2(rot.x, rot.y) * lookSpeed;  // Rotate in y & x axis, spaceship
+
+        // Typical FPS, rotate in y-axis only (xy for camera)
+        // rot.x = Mathf.Clamp(rot.x, -90f, 90f);
+        // transform.eulerAngles = new Vector2(0, rot.y) * lookSpeed;
+        // Camera.main.transform.localRotation = Quaternion.Euler(rot.x * lookSpeed, 0, 0);
+
+        // Move
+        trans = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Debug.Log(transform.rotation);
+        character.Move((transform.rotation * trans) * Time.deltaTime * speed);
+    }
+}
---
+++ Bronchoscopy/README.md
@@ -0,0 +1,13 @@
+# Simple Bronchoscopy Simulation
+Simple simulation for bronchoscopy, with basic features:
+
+- Mouse + WASD control scheme.
+- Character Controller: Smooth navigation inside complex mesh collider.
+- Baked Global Illumination.
+- Model/prefabs using `.fbx` from Blender.
+- Anatomical model generated using [3D Slicer](https://www.slicer.org/).
+- Simple text overlay.
+
+## Requirements
+
+- Unity at least v2021.3.4f1.

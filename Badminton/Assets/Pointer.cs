4711ae0a9f5a28840f038a94165c24e6b707cc6b
Arsenic Bismuth <d.faris323@gmail.com>
2022-12-17 02:21:54 +0100

    Pointer line on primary, UI collider

--- Badminton/Assets/Pointer.cs
+++ Badminton/Assets/Pointer.cs
@@ -9,7 +9,7 @@ public class Pointer : MonoBehaviour
 
     // Parameters
     private int layerMask = 1 << 7; // Bit shift to get a bit mask, only Clickable
-    private int distance = 5;
+    private int distance = 10;
     
     public XRNode inputSource;
     private InputDevice device;
@@ -19,21 +19,38 @@ public class Pointer : MonoBehaviour
         public bool up, down;
     };
     private InputState triggerButton;
+    private InputState primaryButton;
+
+    // Line pointer
+    LineRenderer line;
+    Vector3[] points;
 
     // Start is called before the first frame update
     void Start()
     {
         device = InputDevices.GetDeviceAtXRNode(inputSource);
+
+        // Line from itself (using local space)
+        line = gameObject.GetComponent<LineRenderer>();
+        points = new Vector3[2];
+        points[0] = Vector3.zero;
+        points[1] = new Vector3(0, 0, distance);
+        line.SetPositions(points);
+        line.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        CheckInput();
+        triggerButton = CheckInput(UnityEngine.XR.CommonUsages.triggerButton, triggerButton);
+        primaryButton = CheckInput(UnityEngine.XR.CommonUsages.primaryButton, primaryButton);
         
         // Raycast to interact with buttons
         RaycastHit hit;
+
+        if (primaryButton.state) line.enabled = true;
+        else line.enabled = false;
 
         // Does the ray intersect only object in that layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, layerMask)) {
@@ -59,29 +76,31 @@ public class Pointer : MonoBehaviour
 
     }
 
-    void CheckInput() {
+    InputState CheckInput(InputFeatureUsage<bool> input, InputState output) {
 
         // Try to check value
         bool state;
-        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out state)) {
+        if (device.TryGetFeatureValue(input, out state)) {
             // Store only if valid
-            triggerButton.state = state;
+            output.state = state;
         } else {
             // Master.Log("Input: Fail");
         }
 
         // Check state change
-        if (triggerButton.state != triggerButton.prev) {
-            if (triggerButton.state) triggerButton.down = true;
-            else triggerButton.up = true;
+        if (output.state != output.prev) {
+            if (output.state) output.down = true;
+            else output.up = true;
         } else {
-            triggerButton.up = false;
-            triggerButton.down = false;
+            output.up = false;
+            output.down = false;
         }
-        triggerButton.prev = triggerButton.state;
+        output.prev = output.state;
 
         // For debug, defaults to LCtrl or click
-        if (Input.GetButton("Fire1")) triggerButton.up = true;
+        if (Input.GetButton("Fire1")) output.up = true;
+
+        return output;
     }
 
 }

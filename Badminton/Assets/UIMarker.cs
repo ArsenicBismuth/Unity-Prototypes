5dfd75d769b23d797c2e68877da14b5b828e9195
Arsenic Bismuth <d.faris323@gmail.com>
2025-05-25 18:43:21 +0200

    Reuse hit marks, capped at 50

--- Badminton/Assets/UIMarker.cs
+++ Badminton/Assets/UIMarker.cs
@@ -8,8 +8,10 @@ public class UIMarker : MonoBehaviour
     public GameObject racketUI;
     public Image mark;
     public Color colValid;
+    public int maxMarks = 50;
 
     private Vector3 scale;
+    private Queue<Image> marks = new Queue<Image>();
 
     void Start()
     {
@@ -18,22 +20,34 @@ public class UIMarker : MonoBehaviour
 
     // Get hit position relative to racket center and draw the mark there
     public void Mark(Vector3 pos, bool valid) {
-        Image hit = Instantiate(mark, transform.position, mark.transform.rotation);
-        hit.transform.parent = gameObject.transform;
-        hit.transform.localPosition = new Vector3(-pos.x*scale.x, pos.y*scale.y, 0);
-        hit.transform.localScale = mark.transform.localScale;
-        hit.tag = "UIMark";
+        Image hit;
+        
+        if (marks.Count >= maxMarks) {
+            // Reuse oldest mark
+            hit = marks.Dequeue();
+            hit.gameObject.SetActive(true);
+        } else {
+            // Create new mark
+            hit = Instantiate(mark, transform.position, mark.transform.rotation);
+            hit.transform.parent = gameObject.transform;
+            hit.transform.localScale = mark.transform.localScale;
+            hit.tag = "UIMark";
+        }
 
-        if (valid) hit.color = colValid;
+        // Update position and color
+        hit.transform.localPosition = new Vector3(-pos.x*scale.x, pos.y*scale.y, 0);
+        hit.color = valid ? colValid : mark.color;
+        
+        marks.Enqueue(hit);
     }
 
     [ContextMenu("Clear marks")]
     public void Clear() {
-        foreach (Transform child in transform) {
-            if (child.tag == "UIMark") {
-                Destroy(child.gameObject);
-            }
+        foreach (Image mark in marks) {
+            // Just disable the mark
+            mark.gameObject.SetActive(false);
         }
+        marks.Clear();
     }
 
 }
